f76af9b8f615a055220b1089a4142cdc940dfff4
samira <samira.radmaneshfar@gmail.com>
2025-08-15 13:31:58 +0200

    fix bug

--- src/main/java/com/solvians/showcase/App.java
+++ src/main/java/com/solvians/showcase/App.java
@@ -1,20 +1,26 @@
 package com.solvians.showcase;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
         if (args.length != 2) {
             throw new IllegalArgumentException("Two inputs required (threads, updates). Received:" + args.length);
         }
 
+        try {
             int threads = Integer.parseInt(args[0]);
             int quotes = Integer.parseInt(args[1]);
-
             if (threads <= 0 || quotes <= 0) {
                 throw new IllegalArgumentException("Threads and updates must be positive values");
             }
-
             CertificateUpdateGenerator generator = new CertificateUpdateGenerator(threads, quotes);
             generator.generateCSVLines().forEach(System.out::println);
-            throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + args);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(
+                    "Invalid format for threads or quotes: " + Arrays.toString(args), e
+            );
+        }
     }
+
 }
--- src/test/java/com/solvians/showcase/AppTest.java
+++ src/test/java/com/solvians/showcase/AppTest.java
@@ -1,24 +1,30 @@
 package com.solvians.showcase;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AppTest {
+
     @Test
     public void expectTwoIntArgs() {
-        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
+
+        IllegalArgumentException ex1 = assertThrows(IllegalArgumentException.class, () -> {
             App.main(new String[]{"xxx"});
         });
-        NumberFormatException numbers = Assertions.assertThrows(NumberFormatException.class, () -> {
-            App.main(new String[]{"xxx", "zzz"});
-        });
-        numbers = Assertions.assertThrows(NumberFormatException.class, () -> {
+        assertTrue(ex1.getMessage().contains("Two inputs required"));
+
+        RuntimeException ex2 = assertThrows(RuntimeException.class, () -> {
             App.main(new String[]{"10", "zzz"});
         });
-        assertEquals("For input string: \"zzz\"", numbers.getMessage());
+
+        assertTrue(ex2.getMessage().contains("Invalid format for threads or quotes"));
+
+        assertNotNull(ex2.getCause());
+        assertInstanceOf(NumberFormatException.class, ex2.getCause());
+
+        assertEquals("For input string: \"zzz\"", ex2.getCause().getMessage());
     }
 
 }
